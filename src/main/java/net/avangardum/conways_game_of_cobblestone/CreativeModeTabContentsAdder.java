4ce803924e9882eceb0704394fae237326881f24
Avangardum <yurykuz65@gmail.com>
2024-05-07 18:28:06 +0300

    Removed examples.

--- src/main/java/net/avangardum/conways_game_of_cobblestone/CreativeModeTabContentsAdder.java
+++ src/main/java/net/avangardum/conways_game_of_cobblestone/CreativeModeTabContentsAdder.java
@@ -1,16 +1,14 @@
 package net.avangardum.conways_game_of_cobblestone;
 
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = ConwaysGameOfCobblestone.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = ConwaysGameOfCobblestone.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CreativeModeTabContentsAdder {
     @SubscribeEvent
     public static void addCreativeModeTabContents(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(ConwaysGameOfCobblestone.EXAMPLE_BLOCK_ITEM);
+
     }
 }
