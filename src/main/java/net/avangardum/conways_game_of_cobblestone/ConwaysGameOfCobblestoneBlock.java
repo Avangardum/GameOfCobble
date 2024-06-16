79b837b89e37400b1cb1f5a394d34a07aaa059c4
Avangardum <yurykuz65@gmail.com>
2024-06-16 19:50:18 +0300

    Added inventory to the block entity.

--- src/main/java/net/avangardum/conways_game_of_cobblestone/ConwaysGameOfCobblestoneBlock.java
+++ src/main/java/net/avangardum/conways_game_of_cobblestone/ConwaysGameOfCobblestoneBlock.java
@@ -1,10 +1,17 @@
 package net.avangardum.conways_game_of_cobblestone;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,5 +30,31 @@ public class ConwaysGameOfCobblestoneBlock extends BaseEntityBlock {
     @NotNull
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
+    }
+
+    @Override
+    public void onRemove(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos position,
+            @NotNull BlockState newState, boolean isMovedByPiston) {
+        var blockEntity = getBlockEntity(level, position);
+        blockEntity.dropInventory();
+        super.onRemove(state, level, position, newState, isMovedByPiston);
+    }
+
+    @Override
+    public InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos position,
+            @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hitResult) {
+        if (!level.isClientSide()) {
+            var blockEntity = getBlockEntity(level, position);
+            NetworkHooks.openScreen((ServerPlayer)player, blockEntity, position);
+        }
+
+        return InteractionResult.sidedSuccess(level.isClientSide());
+    }
+
+    @NotNull
+    private ConwaysGameOfCobblestoneBlockEntity getBlockEntity(@NotNull Level level, @NotNull BlockPos position) {
+        var result = (ConwaysGameOfCobblestoneBlockEntity)level.getBlockEntity(position);
+        assert result != null;
+        return result;
     }
 }
--- src/main/java/net/avangardum/conways_game_of_cobblestone/ConwaysGameOfCobblestoneBlockEntity.java
+++ src/main/java/net/avangardum/conways_game_of_cobblestone/ConwaysGameOfCobblestoneBlockEntity.java
@@ -1,11 +1,124 @@
 package net.avangardum.conways_game_of_cobblestone;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.apache.commons.lang3.NotImplementedException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ConwaysGameOfCobblestoneBlockEntity extends BlockEntity {
+public class ConwaysGameOfCobblestoneBlockEntity extends BlockEntity implements MenuProvider {
+    private final int GRID_HEIGHT = 10;
+    private final int GRID_WIDTH = 10;
+    private final int GRID_SIZE = GRID_HEIGHT * GRID_WIDTH;
+
+    private final String INVENTORY_SAVE_KEY = "inventory";
+
+    private final ItemStackHandler itemHandler = new ItemStackHandler(GRID_SIZE) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            setChanged();
+            assert level != null;
+            if (!level.isClientSide()) {
+                level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_ALL);
+            }
+        }
+
+        @Override
+        public boolean isItemValid(int slot, @NotNull ItemStack stack) {
+            return stack.is(Blocks.COBBLESTONE.asItem());
+        }
+    };
+
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+
     public ConwaysGameOfCobblestoneBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(ModBlockEntityTypes.CONWAYS_GAME_OF_COBBLESTONE_BET.get(), blockPos, blockState);
+    }
+
+    @NotNull
+    @Override
+    public Component getDisplayName() {
+        return Component.literal("Conway's Game of Cobblestone");
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int containerId, @NotNull Inventory playerInventory,
+            @NotNull Player player) {
+        throw new NotImplementedException();
+    }
+
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction side) {
+        if (capability == ForgeCapabilities.ITEM_HANDLER) return lazyItemHandler.cast();
+
+        return super.getCapability(capability, side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    @Override
+    public void load(@NotNull CompoundTag pTag) {
+        super.load(pTag);
+        itemHandler.deserializeNBT(pTag.getCompound(INVENTORY_SAVE_KEY));
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @NotNull
+    @Override
+    public CompoundTag getUpdateTag() {
+        return saveWithoutMetadata();
+    }
+
+    public void dropInventory() {
+        var container = new SimpleContainer(GRID_SIZE);
+        for (int i = 0; i < GRID_SIZE; i++) {
+            container.setItem(i, itemHandler.getStackInSlot(i));
+        }
+        assert level != null;
+        Containers.dropContents(level, worldPosition, container);
+    }
+
+    @Override
+    protected void saveAdditional(@NotNull CompoundTag pTag) {
+        pTag.put(INVENTORY_SAVE_KEY, itemHandler.serializeNBT());
+        super.saveAdditional(pTag);
     }
 }
